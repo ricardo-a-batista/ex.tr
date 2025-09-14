79557103dfa70204fe176ae908b1578db3ca74af
Ricardo Batista <iamricardobatista@gmail.com>
2025-09-14 12:26:35 +0100

    chore: init ounce tracing

--- src/instrument.rs
+++ src/instrument.rs
@@ -1,12 +1,18 @@
 use tracing_subscriber::{prelude::*, util::SubscriberInitExt};
 
-pub fn init() {
-    let console_layer = console_subscriber::ConsoleLayer::builder().spawn();
-    let fmt_layer = tracing_subscriber::fmt::layer();
+static INIT: std::sync::Once = std::sync::Once::new();
 
-    tracing_subscriber::registry()
-        .with(console_layer)
-        .with(fmt_layer)
-        .try_init()
-        .ok();
+pub fn init() {
+    INIT.call_once(|| {
+        let console_layer = console_subscriber::ConsoleLayer::builder().spawn();
+        let fmt_layer = tracing_subscriber::fmt::layer();
+
+        tracing_subscriber::registry()
+            .with(console_layer)
+            .with(fmt_layer)
+            .try_init()
+            .unwrap_or_else(|_| {
+                tracing::debug!("tracing already initialized; skipping re-init");
+            });
+    });
 }
